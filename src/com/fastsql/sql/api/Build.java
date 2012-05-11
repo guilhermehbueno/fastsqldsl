19c0867481a4467149d38577b0333d48a6dc6554
Guilherme Henrique Bueno <guilhermehbueno@gmail.com>
2012-05-10 23:59:59 -0300

    sync

--- src/com/fastsql/sql/api/Build.java
+++ src/com/fastsql/sql/api/Build.java
@@ -3,10 +3,10 @@ package com.fastsql.sql.api;
 import com.fastsql.sql.command.result.Result;
 import com.fastsql.sql.command.result.mode.ResultMode;
 
-
 public interface Build {
 	
 	String build();
 	<T> Result<T> build(T retorno);
 	<T> Result<T> build(T retorno, ResultMode mode);
+
 }
