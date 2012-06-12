970d5f8cda419b8fd05ee311620a7d6603ef1535
Guilherme Henrique Bueno <guilhermehbueno@gmail.com>
2012-06-11 23:17:13 -0300

    refatorando pacotes

--- src/com/fastsql/sql/command/expression/LogicalField.java
+++
@@ -1,9 +0,0 @@
-package com.fastsql.sql.command.expression;
-
-import com.fastsql.sql.api.Build;
-
-public interface LogicalField {
-	
-	Build extractExpression();
-
-}
--- src/com/fastsql/sql/command/expression/PredicateExpression.java
+++
@@ -1,7 +0,0 @@
-package com.fastsql.sql.command.expression;
-
-public interface PredicateExpression {
-	
-	public String getValue(String expression);
-
-}
